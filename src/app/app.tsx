fa855825b75fb93ecdf26ca6731e3ed07254ff82
Daniel Engbert <end1@umbc.edu>
2024-09-27 13:09:31 -0400

    more MVP

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-// import NxWelcome from './nx-welcome';
 import MindMap from './MindMap';
 
 export function App() {
   return (
     <div>
       <MindMap />
-      {/* <NxWelcome title="nxwarningdemo" /> */}
     </div>
   );
 }
